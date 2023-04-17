5c45ab8e6772b61871e21375a061388408495691
Yuna Lee <yuna2480@gmail.com>
2023-04-17 01:58:02 -0500

    configfile-jd

--- src/main/rice/parse/ConfigFile.java
+++ src/main/rice/parse/ConfigFile.java
@@ -4,29 +4,47 @@ import main.rice.node.APyNode;
 
 import java.util.List;
 
+/**
+ * Represents a configuration file with a function name, a list of APyNodes, and a number of random values
+ */
 public class ConfigFile {
     private String funcName;
     private List<APyNode<?>> nodes;
     private int numRand;
 
-    // Constructor
+    /**
+     * Constructor for ConfigFile
+     *
+     * @param funcName The name of the function under test
+     * @param nodes The list of PyNodes that will be used to generate TestCases for the function under test
+     * @param numRand The number of random test cases to be generated
+     */
     public ConfigFile(String funcName, List<APyNode<?>> nodes, int numRand) {
         this.funcName = funcName;
         this.nodes = nodes;
         this.numRand = numRand;
     }
 
-    // Getter for funcName
+    /**
+     * Returns the name of the function under test
+     @return The function name
+     */
     public String getFuncName() {
         return funcName;
     }
 
-    // Getter for nodes
+    /**
+     * Returns the List of PyNodes that will be used to generate TestCases for the function under test
+     * @return The list of APyNodes
+     */
     public List<APyNode<?>> getNodes() {
         return nodes;
     }
 
-    // Getter for numRand
+    /**
+     * Returns the number of random test cases to be generated.
+     * @return The number of random test cases
+     */
     public int getNumRand() {
         return numRand;
     }
