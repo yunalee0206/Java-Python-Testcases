200be6dfecece336a28286a0197a8e900626d420
Yuna Lee <yuna2480@gmail.com>
2023-04-16 23:08:44 -0500

    configfile

--- src/main/rice/parse/ConfigFile.java
+++ src/main/rice/parse/ConfigFile.java
@@ -1,3 +1,33 @@
 package main.rice.parse;
 
-// TODO: implement the ConfigFile class here
+import main.rice.node.APyNode;
+
+import java.util.List;
+
+public class ConfigFile {
+    private String funcName;
+    private List<APyNode<?>> nodes;
+    private int numRand;
+
+    // Constructor
+    public ConfigFile(String funcName, List<APyNode<?>> nodes, int numRand) {
+        this.funcName = funcName;
+        this.nodes = nodes;
+        this.numRand = numRand;
+    }
+
+    // Getter for funcName
+    public String getFuncName() {
+        return funcName;
+    }
+
+    // Getter for nodes
+    public List<APyNode<?>> getNodes() {
+        return nodes;
+    }
+
+    // Getter for numRand
+    public int getNumRand() {
+        return numRand;
+    }
+}
